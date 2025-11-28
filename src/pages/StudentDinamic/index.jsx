e99988e37a60632f4f928c86e619e28d8d49626c
Wesleybezerra3 <wesleybezerra770@gmail.com>
2025-11-27 22:35:08 -0300

    ...

--- src/pages/StudentDinamic/index.jsx
+++ src/pages/StudentDinamic/index.jsx
@@ -4,13 +4,16 @@ import { useEffect } from "react";
 import api from "../../services/api";
 import { useState } from "react";
 import { useAppContext } from "../../context/AppContext";
-import { AwardIcon, Edit } from "lucide-react";
+import { ArrowLeftRight, AwardIcon, Edit, GraduationCapIcon, KeyRound, Replace, SwitchCamera, ToggleLeft } from "lucide-react";
 import { Load } from "../../componets/Loards/load";
+import { ChangeClass } from "../../componets/Modals/ChangeClass";
 
 export const StudentDinamic = () => {
   const { setNotificationMessage, setResetKey, setTypeNotification, refresh } =
     useAppContext();
   const [load, setLoad] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+  
 
   const { matricula } = useParams();
   const [isEdit, setIsEdit] = useState(true);
@@ -97,159 +100,171 @@ export const StudentDinamic = () => {
 
   return (
     <>
+    <ChangeClass visible={isOpen} onClose={() => setIsOpen(false)}/>
       <section className="container">
         <section className={style.info}>
-          <form onSubmit={handleForm}>
-            <div className={style.headerInfo}>
-              <h1>Informações pessoais</h1>
-              <button
-                type="button"
-                onClick={() => {
-                  setIsEdit((prev) => !prev);
-                }}
-              >
-                <Edit />
-              </button>
+          <div className={style.headerInfo}>
+            <h1>Informações pessoais</h1>
+            <button
+              type="button"
+              onClick={() => {
+                setIsEdit((prev) => !prev);
+              }}
+            >
+              <Edit />
+            </button>
+          </div>
+          <div className={style.content}>
+            <div>
+              <form onSubmit={handleForm}>
+                <div className={style.containerInfo}>
+                  <div className={style.containerInput}>
+                    <label htmlFor="nome">Nome</label>
+                    <input
+                      type="text"
+                      name="nome"
+                      value={formData.nome}
+                      onChange={handleObj}
+                      disabled={isEdit}
+                      autoComplete="false"
+                    />
+                    <label htmlFor="data">Data de nascimento</label>
+                    <input
+                      type="date"
+                      name="data_nascimento"
+                      onChange={handleObj}
+                      value={formData.data_nascimento}
+                      disabled={isEdit}
+                    />
+                    <label htmlFor="cpf">CPF</label>
+                    <input
+                      type="text"
+                      name="cpf"
+                      onChange={handleObj}
+                      value={formData.cpf}
+                      disabled={isEdit}
+                    />
+                  </div>
+                  <div className={style.containerInput}>
+                    <label htmlFor="matricula">Matrícula</label>
+                    <input
+                      type="text"
+                      name="matricula"
+                      value={matricula}
+                      disabled
+                    />
+                    <label htmlFor="email">E-mail</label>
+                    <input
+                      type="email"
+                      name="email"
+                      onChange={handleObj}
+                      value={formData.email}
+                      disabled={isEdit}
+                    />
+                    {/* <label htmlFor="telefone">Telefone</label>
+                <input type="text" name="telefone" /> */}
+                  </div>
+                </div>
+              </form>
             </div>
-            <div className={style.containerInfo}>
-              <div className={style.containerInput}>
-                <label htmlFor="nome">Nome</label>
-                <input
-                  type="text"
-                  name="nome"
-                  value={formData.nome}
-                  onChange={handleObj}
-                  disabled={isEdit}
-                  autoComplete="false"
-                />
-
-                <label htmlFor="data">Data de nascimento</label>
-                <input
-                  type="date"
-                  name="data_nascimento"
-                  onChange={handleObj}
-                  value={formData.data_nascimento}
-                  disabled={isEdit}
-                />
-                <label htmlFor="cpf">CPF</label>
-                <input
-                  type="text"
-                  name="cpf"
-                  onChange={handleObj}
-                  value={formData.cpf}
-                  disabled={isEdit}
-                />
+            <div className={style.containerActions}>
+              <h1>Ações</h1>
+              <div className={style.actions}>
+                <article className={style.cardActions} onClick={()=>{
+                      setIsOpen(true)
+                    }}>
+                  <div>
+                    <Replace className={style.icon}  />
+                  </div>
+                  <div>Mudar Turma</div>
+                </article>
+                <article className={style.cardActions}>
+                  <div>
+                    <GraduationCapIcon className={style.icon} />
+                    <ArrowLeftRight className={style.icon} />
+                  </div>
+                  <div>Mudar Curso</div>
+                </article>
+                <article className={style.cardActions}>
+                  <div>
+                    <ToggleLeft className={style.icon} />
+                  </div>
+                  <div>Alterar Situação</div>
+                </article>
+                <article className={style.cardActions}>
+                  <div>
+                    <KeyRound className={style.icon} />
+                  </div>
+                  <div>Resetar Senha</div>
+                </article>
               </div>
-
-              <div className={style.containerInput}>
-                <label htmlFor="matricula">Matrícula</label>
-                <input
-                  type="text"
-                  name="matricula"
-                  value={matricula}
-                  disabled
-                />
-
-                <label htmlFor="email">E-mail</label>
-                <input
-                  type="email"
-                  name="email"
-                  onChange={handleObj}
-                  value={formData.email}
-                  disabled={isEdit}
-                />
-                {/* <label htmlFor="telefone">Telefone</label>
-              <input type="text" name="telefone" /> */}
-              </div>
             </div>
-            <div className={style.containerBtn}>
-              <button
-                type="button"
-                className={style.btnC}
-                disabled={isEdit}
-                onClick={() => {
-                  setFormData(student);
-                  setIsEdit(true)
-                }}
-              >
-                Cancelar
-              </button>
-              <button type="submit" className={style.btn} disabled={isEdit}>
-                <Load visible={load} />
-                {load ? "" : "Salvar"}
-              </button>
-            </div>
-          </form>
-  
+          </div>
+          <div className={style.containerBtn}>
+            <button
+              type="button"
+              className={style.btnC}
+              disabled={isEdit}
+              onClick={() => {
+                setFormData(student);
+                setIsEdit(true);
+              }}
+            >
+              Cancelar
+            </button>
+            <button type="submit" className={style.btn} disabled={isEdit}>
+              <Load visible={load} />
+              {load ? "" : "Salvar"}
+            </button>
+          </div>
         </section>
 
         <section className={style.containerInfoAca}>
-           <div className={style.card}> 
+          <div className={style.card}>
             <div>
-              <h1>
-                Informações academicas
-              </h1>
+              <h1>Informações academicas</h1>
             </div>
             <div className={style.containerInfos}>
-              
-                <div className={style.infos}>
-                  <p>
-                    Curso
-                  </p>
-                  <p>
-                    {student?.turma?.curso?.nome}
-                  </p>
-                </div>
-                <div className={style.infos}>
-                  <p>
-                    Turno
-                  </p>
-                  <p>
-                    {student?.turma?.turno}
-                  </p>
-                </div>
+              <div className={style.infos}>
+                <p>Curso</p>
+                <p>{student?.turma?.curso?.nome}</p>
+              </div>
+              <div className={style.infos}>
+                <p>Turno</p>
+                <p>{student?.turma?.turno}</p>
+              </div>
 
-                  <div className={style.infos}>
-                  <p>
-                    Semestre/Periodo
-                  </p>
-                  <p>
-                    {student?.turma?.semestre}
-                  </p>
-                </div>
+              <div className={style.infos}>
+                <p>Semestre/Periodo</p>
+                <p>{student?.turma?.semestre}</p>
+              </div>
 
-                  <div className={style.infos}>
-                  <p>
-                    Situação
-                  </p>
-                  <p style={{color: student?.situacao ==='ativo'?'green':"red"}}>
-                    {student?.situacao}
-                  </p>
-                </div>
-                  <div className={style.infos}>
-                  <p>
-                    Turma
-                  </p>
-                  <p>
-                    {student?.turma?.nome}
-                  </p>
-                </div>
+              <div className={style.infos}>
+                <p>Situação</p>
+                <p
+                  style={{
+                    color: student?.situacao === "ativo" ? "green" : "red",
+                  }}
+                >
+                  {student?.situacao}
+                </p>
+              </div>
+              <div className={style.infos}>
+                <p>Turma</p>
+                <p>{student?.turma?.nome}</p>
+              </div>
             </div>
-           </div>
-           <div className={style.card}>
+          </div>
+          <div className={style.card}>
             <div>
               <h1>Resumo</h1>
             </div>
             <div>
-              <button>
-                Presença
-              </button>
+              <button>Presença</button>
 
-              <button>
-                Nota
-              </button>
+              <button>Nota</button>
             </div>
-           </div>
+          </div>
         </section>
       </section>
     </>
